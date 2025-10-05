a1afbf5ad83f1e5707e1c32138d5db10c131dff3
mobsenpai <work.velocity806@passinbox.com>
2025-10-06 01:15:22 +0530

    steps to deploy on gihtub pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import AuthProvider from "./contexts/AuthContext";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/dashboard-test">
         <AuthProvider>
           <AppRoutes />
         </AuthProvider>
